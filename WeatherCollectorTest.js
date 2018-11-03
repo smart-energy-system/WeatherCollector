80eaba8322d2863559b0db4b09f4330f2a6856f7
spethso <spethso@studi.informatik.uni-stuttgart.de>
2018-11-03 17:49:38 +0100

    Add modes to collect or get data in WeatherCollectorTest.js

--- WeatherCollectorTest.js
+++ WeatherCollectorTest.js
@@ -3,12 +3,13 @@ const DBController = require('./DBController');
 const fs = require('fs');
 let config;
 function init(callback) {
-    const args = process.argv.slice(2);
-    if (args.length === 3) {
+    const args = process.argv.slice(3);
+    if (args.length === 4) {
         config = {
             apiToken: args[0],
             lat: args[1],
-            lon: args[2]
+            lon: args[2],
+            mode: args[3]
         };
         fs.writeFileSync('./testconfig.json', JSON.stringify(config), 'utf8', (err) => {
             console.log('[Error] Error while writing test config file');
@@ -18,17 +19,29 @@ function init(callback) {
         config = JSON.parse(fs.readFileSync('./testconfig.json', 'utf-8', (err) => {
             console.log('[Error] Error while reading test config file');
         }));
+
+        if(args.length === 1) {
+            config.mode = args[0];
+        }
         callback(config);
+    } else {
+        callback(null);
     }
 }
 
 init((config) => {
-    const weatherCollector = new WeatherCollector(config.apiToken, config.lat, config.lon);
-    const db = new DBController();
-    
-    
-    // weatherCollector.retrieveWeatherAndSave();
-    db.getLatestWeatherFromDBByCoordinates(config.lat, config.lon, (result) => {
-        console.log(result);
-    });
+    if (config === null) {
+        console.log('Config data is not given');
+    } else {
+        const weatherCollector = new WeatherCollector(config.apiToken, config.lat, config.lon);
+        const db = new DBController();
+
+        if (config.mode == 'collect') {
+            weatherCollector.retrieveWeatherAndSave();
+        } else if (config.mode == 'get') {
+            db.getLatestWeatherFromDBByCoordinates(config.lat, config.lon, (result) => {
+                console.log(result);
+            });
+        }   
+    }
 });
